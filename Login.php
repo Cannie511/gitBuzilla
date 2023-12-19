381e3775cb7da87e4ed1d17b59ca4f34062063df
Cannie511 <Canh51102@gmail.com>
2023-12-19 18:00:06 +0700

    xoa script

--- Login.php
+++ Login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,6 +26,7 @@
     }
   </style>
 </head>
+
 <body>
 
   <div class="form-container">
@@ -54,17 +56,18 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
   <script>
     function login() {
-            // Lấy giá trị từ các trường input
-            var email = document.getElementById("email").value;
-            var password = document.getElementById("password").value;
+      // Lấy giá trị từ các trường input
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
 
-            // Kiểm tra điều kiện đăng nhập (đây chỉ là ví dụ đơn giản)
-            if (email === "admin" && password === "123") {
-                window.location.href = "index-logined.php";
-            } else {
-                alert("Tài khoản hoặc mật khẩu không đúng!");
-            }
-        }
+      // Kiểm tra điều kiện đăng nhập (đây chỉ là ví dụ đơn giản)
+      if (email === "admin" && password === "123") {
+        window.location.href = "index-logined.php";
+      } else {
+        alert("Tài khoản hoặc mật khẩu không đúng!");
+      }
+    }
   </script>
 </body>
+
 </html>
